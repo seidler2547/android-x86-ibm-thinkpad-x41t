9ad2ef74dc0ab28d0e13cf9252d515e073f68e53
Stefan Seidel <android@stefanseidel.info>
2012-01-27 23:44:40 +0100

    initial commit

---
+++ AndroidBoard.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/x41t_defconfig
+TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/x41t_info
+#TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
+
+include $(GENERIC_X86_ANDROID_MK)
---
+++ x41t.mk
@@ -0,0 +1,23 @@
+PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
+#PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT)
+PRODUCT_PACKAGES += lights.$(TARGET_PRODUCT)
+PRODUCT_PACKAGES += wacom-input
+
+#PRODUCT_COPY_FILES := \
+#    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
+
+$(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
+
+USE_SQUASHFS := 0
+
+PRODUCT_NAME := thinkpad_x41t
+PRODUCT_DEVICE := x41t
+PRODUCT_MANUFACTURER := ibm
+
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlays
+
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/init.x41t.sh:system/etc/init.x41t.sh
+#	$(LOCAL_PATH)/asound.conf:system/etc/asound.conf
+
+include $(call all-subdir-makefiles)
