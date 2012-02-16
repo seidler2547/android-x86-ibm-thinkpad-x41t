765e016601aad682a7ce9a4df79a2741f93d179b
Stefan Seidel <android@stefanseidel.info>
2012-02-16 20:55:03 +0100

    shell script and patches for complete build: build own toolchain and patch kernel suspend

---
+++ patches/apply.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# sanity test
+[ -d prebuilt ] || { echo "You need to run this script from the top of an Android source tree"; exit 1; }
+
+MYDIR="$(readlink -f $(dirname $0))"
+
+# build toolchain
+[ -e prebuilt/linux-x86/toolchain/i686-android-linux-4.4.3/build-toolchain.sh ] && {
+    cd prebuilt
+    git apply "$MYDIR/prebuilt.patch"
+    cd ../ndk
+    git apply "$MYDIR/ndk.patch"
+    cd ..
+    prebuilt/linux-x86/toolchain/i686-android-linux-4.4.3/build-toolchain.sh
+}
+
+# apply kernel patches
+cd kernel
+git status kernel/power/suspend.c | tail -1 | grep -q 'nothing to commit' && git apply "$MYDIR/kernel-suspend.patch"
+cd ..
