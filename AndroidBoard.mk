911844bf75b0e46b9aaebbae2bafd18bbc7c6490
Stefan Seidel <android@stefanseidel.info>
2012-03-04 21:39:44 +0100

    move wakeup_button kernel module to it's own directory and add phc-intel as another sample of external kernel modules

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,5 +3,6 @@ TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/x41t_defconfig
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/x41t_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 $(call add-prebuilt-targets,$(TARGET_OUT),$(TARGET_PREBUILT_APPS))
+TARGET_EXTRA_KERNEL_MODULES := wakeup_button phc-intel
 
 include $(GENERIC_X86_ANDROID_MK)
