530a248a807b37b74309d3c1cea9cf0ca6cd28de
Stefan Seidel <android@stefanseidel.info>
2012-03-05 11:45:32 +0100

    change hdaps from mainline kernel to tp_smapi

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,6 +3,6 @@ TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/x41t_defconfig
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/x41t_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 $(call add-prebuilt-targets,$(TARGET_OUT),$(TARGET_PREBUILT_APPS))
-TARGET_EXTRA_KERNEL_MODULES := wakeup_button phc-intel
+TARGET_EXTRA_KERNEL_MODULES := wakeup_button phc-intel tp_smapi
 
 include $(GENERIC_X86_ANDROID_MK)
